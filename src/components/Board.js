c7270f09572d4c4b4c81555b73a09304cddd2393
Daniel <danwater1@gmail.com>
2018-04-01 15:51:45 -0700

    Added box background

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addChip } from '../actions/game';
-import { Stage, Layer } from 'react-konva';
+import { Rect, Stage, Layer } from 'react-konva';
 import Chip from './Chip';
 
 class Board extends Component {
@@ -14,9 +14,22 @@ class Board extends Component {
     return (
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
+        <Rect
+        x={0}
+        y={50}
+        width={500}
+        height={500}
+        fill='brown'
+      />
           {this.props.chips.map((row, rowIndex) => {
             return row.map((col, colIndex) => {
-              return <Chip key={rowIndex + " " + colIndex} value={col} player={col} x={colIndex * 50+25} y={rowIndex * -50+500} onClick={(e) => this.props.addChip(colIndex, e)}/>
+              return <Chip 
+                key={rowIndex + " " + colIndex} 
+                value={col} 
+                player={col}
+                x={colIndex * 50+25}
+                y={rowIndex * -50+525}
+                onClick={(e) => this.props.addChip(colIndex, e)}/>
               });
             })}
         </Layer>
--- src/components/Chip.js
+++ src/components/Chip.js
@@ -3,7 +3,7 @@ import { Circle } from 'react-konva'
 import Konva from 'konva'
 
 const Chip = (props) => (
-  <Circle x={props.x} y={props.y} radius={25} fill={props.player === 1 ? 'red' : 'blue'} onClick={props.onClick}/>
+  <Circle x={props.x} y={props.y} radius={25} fill={props.player === 1 ? 'red' : 'white'} onClick={props.onClick}/>
 );
 
 export default Chip;
