5cb28d810570064d701b37a134fa127aa49688e6
Daniel Waters <danwater1@gmail.com>
2018-04-10 16:35:50 -0700

    moved chip and selectors to their own functions

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import Chip from './Chip';
 import ChipSelector from './ChipSelector';
 import { connect } from 'react-redux';
+import { WIDTH } from '../utils/constants';
 
-const Board = (props) => (
-  <div className="board">
-    <ChipSelector col={0} classes="chip-selector select-0" />
-    <ChipSelector col={1} classes="chip-selector select-1" />
-    <ChipSelector col={2} classes="chip-selector select-2" />
-    <ChipSelector col={3} classes="chip-selector select-3" />
-    <ChipSelector col={4} classes="chip-selector select-4" />
-    <ChipSelector col={5} classes="chip-selector select-5" />
-    <ChipSelector col={6} classes="chip-selector select-6" />
-    {props.chips.map((row, rowIndex) => {
+class Board extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  createChipSelectors() {
+    let chipSelectors = [];
+    for (let i = 0; i < WIDTH; i++) {
+      chipSelectors.push(<ChipSelector key={i} col={i} classes={`chip-selector chip-selector--player${this.props.currentPlayer} select-${i}`} />);
+    }
+    return chipSelectors;
+  }
+
+  createChips() {
+    let chips = [];
+    this.props.chips.map((row, rowIndex) => {
       return row.map((col, colIndex) => {
         if (col === 1) {
-          return <Chip key={rowIndex + " - " + colIndex} classes={"chip chip--red loc-" + colIndex + "-" + rowIndex} />
+          chips.push(<Chip key={rowIndex + " - " + colIndex} classes={"chip chip--red loc-" + colIndex + "-" + rowIndex} />);
         } else if (col === 2) {
-          return <Chip key={rowIndex + " - " + colIndex} classes={"chip chip--yellow loc-" + colIndex + "-" + rowIndex} />
+          chips.push(<Chip key={rowIndex + " - " + colIndex} classes={"chip chip--yellow loc-" + colIndex + "-" + rowIndex} />);
         }
       })
-    })}
-  </div>
-);
+    })
+    return chips;
+  }
+
+  render() {
+    return (
+      <div className="board">
+        {this.createChipSelectors()}
+        {this.createChips()}
+      </div>
+    )
+  }
+};
 
 const mapStateToProps = (state, props) => ({
-  chips: state.game.chips
+  chips: state.game.chips,
+  currentPlayer: state.game.currentPlayer
 });
 
 export default connect(mapStateToProps)(Board);
